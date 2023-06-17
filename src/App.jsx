bde6b2efc996339c0b800f8a71ce2e201afa650a
vishnuvyasankp <20b128@cev.ac.in>
2023-06-17 13:03:57 +0530

    seventh commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Route, Routes } from "react-router-dom"
+import  Home  from './pages/Home'
+import Contact  from  './pages/Contact'
+import About from './pages/About'
+import Notfound from './pages/Notfound'
+import Header from './units/Header'
+
+export default function App() {
+  return (
+    <>
+    <Header/>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="*" element={<Notfound />} />
+    </Routes>
+    </>
+  )
+}
---
+++ src/units/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Route, Routes, Link } from "react-router-dom"
+
+
+export default function Header() {
+  return (
+    <div>
+         <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+            </li>
+            <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+    </div>
+  )
+}
+
